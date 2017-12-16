9b16c03fd0751ba618faceba7e8b858eaaeda1d4
Erick Roberto Mercado Arroyo <erick.mercado@dinerio.mx>
2017-12-15 22:21:58 -0600

    mapped database

--- src/main/groovy/mx/finerio/api/domain/User.groovy
+++ src/main/groovy/mx/finerio/api/domain/User.groovy
@@ -8,50 +8,34 @@ import groovy.transform.ToString
 import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
-@Table(name = 'users')
+@Table(name = 'user')
 @ToString(includePackage = false, includeNames = true, includes = ['username'])
 class User implements UserDetails {
   
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = 'id', nullable = false, updatable = false)
-  Long id
+  String id
 
-  @NotNull(message = 'user.username.null')
-  @Size(min = 1, max = 100, message = 'user.username.size')
-  @Column(name = 'username', nullable = false, unique = true, length = 100)
+  @Column(name = 'email', nullable = false, unique = true)
   String username
 
-  @NotNull(message = 'user.password.null')
-  @Size(min = 1, max = 255, message = 'user.password.size')
   @Column(name = 'password', nullable = false)
   String password
 
-  @Column(name = 'enabled', nullable = false)
+  @Column(name = 'enabled', nullable = true)
   boolean enabled
 
-  @Column(name = 'account_non_expired', nullable = false)
+  @Column(name = 'account_non_expired', nullable = true)
   boolean accountNonExpired
 
-  @Column(name = 'account_non_locked', nullable = false)
+  @Column(name = 'account_non_locked', nullable = true)
   boolean accountNonLocked
 
-  @Column(name = 'credentials_non_expired', nullable = false)
+  @Column(name = 'credentials_non_expired', nullable = true)
   boolean credentialsNonExpired
 
   @Transient
   List authorities
-
-  @NotNull(message = 'user.dateCreated.null')
-  @Column(name = 'date_created')
-  Date dateCreated
-
-  @NotNull(message = 'user.lastUpdated.null')
-  @Column(name = 'last_updated')
-  Date lastUpdated
-
-  @Column(name = 'date_deleted', nullable = true)
-  Date dateDeleted
 
   List getJsonAttributes() {
     [ 'username', 'authorities' ]
