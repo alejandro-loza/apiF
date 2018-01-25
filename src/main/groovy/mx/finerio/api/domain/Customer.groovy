134e4d439bc1071ca85cb737a78c5c1e0c204a6f
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-01-25 15:47:02 -0600

    Added 'Customer' entity

---
+++ src/main/groovy/mx/finerio/api/domain/Customer.groovy
@@ -0,0 +1,28 @@
+package mx.finerio.api.domain
+
+import javax.persistence.*
+import javax.validation.constraints.*
+
+import groovy.transform.ToString
+
+@Entity
+@Table(name = 'customers')
+@ToString(includePackage = false, includeNames = true)
+class Customer {
+  
+  @Id
+  @NotNull
+  @Column(name = 'id', updatable = false)
+  String id
+
+  @NotNull
+  @Size(min = 1, max = 50)
+  @Column(name = 'name')
+  String name
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = 'client_id')
+  Client client
+
+}
