f0de7fbd350b4eb0899ed9175bce47410f969a5d
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-01-25 12:20:39 -0600

    Added 'Client' entity for OAuth2

---
+++ src/main/groovy/mx/finerio/api/domain/repository/ClientRepository.groovy
@@ -0,0 +1,12 @@
+package mx.finerio.api.domain.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+
+import mx.finerio.api.domain.*
+
+interface ClientRepository extends JpaRepository<Client, Long>, JpaSpecificationExecutor {
+  
+  Client findOneByUsername( String username )
+
+}
