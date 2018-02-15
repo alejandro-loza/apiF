025a6a5d416e87d3edd7908314c1dcc00a6483ab
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-02-15 16:35:13 -0600

    Added 'MovementService.createAll()' to ScraperCallbackController.transactions()

--- src/main/groovy/mx/finerio/api/dtos/TransactionData.groovy
+++ src/main/groovy/mx/finerio/api/dtos/TransactionData.groovy
@@ -5,6 +5,7 @@ import groovy.transform.ToString
 @ToString(includePackage = false, includeNames = true)
 class TransactionData {
 
+  String credential_id
   String account_id
   List<Transaction> transactions
 
--- src/main/groovy/mx/finerio/api/services/MovementService.groovy
+++ src/main/groovy/mx/finerio/api/services/MovementService.groovy
@@ -41,6 +41,16 @@ class MovementService {
 
   }
 
+  void createConcept( Movement movement ) throws Exception {
+
+    def conceptData = [
+      description: movement.description,
+      amount: movement.amount,
+      type: Concept.Type.DEFAULT ]
+    conceptService.create( movement.id, conceptData )
+
+  }
+
   private Movement create( Account account, Transaction transaction )
       throws Exception {
 
@@ -70,66 +80,6 @@ class MovementService {
     }
 
     null
-
-  }
-
-  Movement createMovement(Map params){
-    def credential = credentialPersistenceService.findOne( params.request.credential_id )
-
-    if ( !credential ) {
-      throw new InstanceNotFoundException(
-          'movement.createMovement.credential.null' )
-    }
-    def account = accountService.findById( params.request.account_id )
-    if ( !account ) {
-      throw new InstanceNotFoundException(
-          'movement.createMovement.account.null' )
-    }
-    def transactionsSize = params.request.transactions.size
-    for( int i=0; i < transactionsSize; i++ ){
-       def date = new Date().parse("yyyy-MM-dd'T'HH:mm:ss",  params.request.transactions[i].made_on ) ?: new Date()
-       def txAmount = params.request.transactions[ i ].amount
-       def amount = new BigDecimal( txAmount ).abs().setScale( 2, BigDecimal.ROUND_HALF_UP )
-       def type = txAmount < 0 ? Movement.Type.CHARGE : Movement.Type.DEPOSIT
-       def descriptionFinal = params.request.transactions[ i ].description.take(255) 
-       def params2 = [
-            date: date,
-            description: descriptionFinal,
-            amount: amount,
-            type: type,
-            account: account
-       ]
-       def instance = movementRepository.findByDateAndDescriptionAndAmountAndTypeAndAccount(
-	params2.date, 
-	params2.description, 
-	params2.amount, 
-	params2.type, 
-	params2.account )
-       def movement = instance ?: new Movement()
-       movement.dateCreated = movement.dateCreated ?: new Date()
-       movement.lastUpdated = new Date()
-       movement.version = 0 
-       movement.account = account
-       movement.date = date
-       movement.description = descriptionFinal
-       movement.customDate = date
-       movement.customDescription = descriptionFinal
-       movement.amount = amount
-       movement.balance = amount
-       movement.type = type
-       movementRepository.save(movement)
-       if ( !instance ) createConcept( movement )
-       movement
-    }
-  }
-
-  private void createConcept( Movement movement ) throws Exception {
-
-    def conceptData = [
-      description: movement.description,
-      amount: movement.amount,
-      type: Concept.Type.DEFAULT ]
-    conceptService.create( movement.id, conceptData )
 
   }
 
