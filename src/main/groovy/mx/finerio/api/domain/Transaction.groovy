20126b790b5acc9c87b29e937aca460d4b738d62
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-03-14 12:34:44 -0600

    Added 'Transaction' entity

---
+++ src/main/groovy/mx/finerio/api/domain/Transaction.groovy
@@ -0,0 +1,49 @@
+package mx.finerio.api.domain
+
+import java.sql.Timestamp
+
+import javax.persistence.*
+import javax.validation.constraints.*
+
+import groovy.transform.ToString
+import org.hibernate.annotations.GenericGenerator
+
+@Entity
+@Table(name = 'transactions')
+@ToString(includeNames = true, includePackage = false)
+public class Transaction {
+
+  @Id @GeneratedValue
+  @Column(name = 'id', updatable = false)
+  Long id
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = 'account_id', nullable = false)
+  Account account
+  
+  @Column(name = 'bank_date', nullable = false)
+  Timestamp bankDate
+
+  @Column(name = 'description', nullable = false, length = 255)
+  String description
+
+  @Column(name = 'amount', nullable = false)
+  BigDecimal amount
+
+  @Column(name = 'charge', nullable = false)
+  Boolean charge
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = 'category_id', nullable = true)
+  Category category
+  
+  @Column(name = 'date_created', nullable = false)
+  Timestamp dateCreated
+
+  @Column(name = 'last_updated', nullable = false)
+  Timestamp lastUpdated
+
+  @Column(name = 'date_deleted', nullable = true)
+  Timestamp dateDeleted
+
+}
