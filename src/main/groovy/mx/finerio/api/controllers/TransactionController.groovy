d730db91250d1e84a30d9575e20d9aa1fd692f4a
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-03-15 17:41:45 -0600

    Replaced 'MovementController' with 'TransactionController'

--- src/main/groovy/mx/finerio/api/controllers/TransactionController.groovy
+++ src/main/groovy/mx/finerio/api/controllers/TransactionController.groovy
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class MovementController {
+class TransactionController {
 
   @Autowired
   TransactionService transactionService
