3fe249da68e7330f1f7596116e728c0c316114c7
Jose Antonio Vargas <jose.vargas@finerio.mx>
2018-10-16 16:33:31 -0500

    Se sube restructuracion y se agrega la validacion para movimientos no considerados

--- sql/sp_update_amount.sql
+++ sql/sp_update_amount.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS sp_change_on_movement;
 CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_concepts bit(1),IN new_amount decimal(19,2),IN old_amount decimal(19,2),
                                         IN new_category_id varchar(255),IN old_category_id varchar(255),IN new_type varchar(255),IN old_type varchar(255),
                                         IN new_custom_date datetime,IN old_custom_date datetime, IN new_account_id varchar(255),IN new_id varchar(255),
-                                        IN new_date_deleted datetime,IN v_daysinit int(11), IN v_daysfinal int(11))
+                                        IN new_date_deleted datetime, IN new_duplicated bit(1),IN old_duplicated bit(1), IN v_daysinit int(11), IN v_daysfinal int(11))
   BEGIN
 
   DECLARE v_ch_has_concepts INT(11);
@@ -12,18 +12,11 @@ CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_co
   DECLARE v_ch_date INT(11);
   DECLARE v_ch_type INT(11);
   DECLARE v_ch_category INT(11);
-  DECLARE v_id_movement_stat bigint(20);
-  DECLARE v_amount_movement_stat decimal(19,2);
-  DECLARE v_userid VARCHAR (255);
-  DECLARE v_initdate datetime;
-  DECLARE v_finaldate datetime;
-  DECLARE v_new_id_movement_stat bigint(20);
-  DECLARE v_new_amount_movement_stat decimal(19,2);
+  DECLARE v_ch_duplicated INT(11);
+
   #Variables for concepts
   DECLARE v_category_id VARCHAR(255);
   DECLARE v_amount_concept decimal(19,2);
-  DECLARE vn_id_movement_stat bigint(20);
-  DECLARE vn_amount_movement_stat decimal(19,2);
   DECLARE done INT DEFAULT FALSE;
   DEClARE concept_cursor CURSOR FOR SELECT category_id, amount FROM concept WHERE movement_id=new_id;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -33,58 +26,28 @@ CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_co
   SET v_ch_date=new_custom_date != old_custom_date;
   SET v_ch_type=new_type != old_type;
   SET v_ch_category=new_category_id != old_category_id;
+  SET v_ch_duplicated=new_duplicated != old_duplicated;
  
   #If user add or remove concepts
   IF v_ch_has_concepts AND new_has_concepts  THEN
 
-    CALL sp_get_movementstat_data_frommovement (new_account_id,old_category_id,new_custom_date,new_type,v_daysinit,
-                                                v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
-
-    UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat-new_amount) WHERE ms.id=v_id_movement_stat;
+  CALL sp_add_substract(new_id, old_category_id, new_amount,new_type,new_custom_date,new_duplicated,'MINUS',v_daysinit,v_daysfinal);
 
   ELSEIF v_ch_has_concepts AND NOT new_has_concepts  THEN
 
-    CALL sp_get_movementstat_data_frommovement (new_account_id,new_category_id,new_custom_date,new_type,v_daysinit,
-                                                v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
-
-    IF v_amount_movement_stat IS NULL THEN
-
-      CALL sp_get_initandfinaldate ( new_custom_date ,v_daysinit,v_daysfinal, v_initdate,v_finaldate);
-      INSERT INTO movement_stat(user_id,category_id,type,amount,init_date,final_date) 
-      values(v_userid,new_category_id,new_type,new_amount,v_initdate,v_finaldate);
-
-    ELSE
-
-      UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat+new_amount) WHERE ms.id=v_id_movement_stat;
-
-    END IF;
+   CALL sp_add_substract(new_id, new_category_id, new_amount,new_type,new_custom_date,new_duplicated,'PLUS',v_daysinit,v_daysfinal);
 
   END IF;
 
-  # If user change amount or date or category while it does not have concepts
-   IF ((v_ch_amount OR v_ch_date OR v_ch_type OR v_ch_category OR v_ch_category IS NULL) AND (NOT v_ch_has_concepts AND NOT new_has_concepts)) THEN
+  # If user change amount or date or category, or duplicated while it does not have concepts
+   IF ((v_ch_amount OR v_ch_date OR v_ch_type OR v_ch_duplicated OR v_ch_category OR v_ch_category IS NULL) AND (NOT v_ch_has_concepts AND NOT new_has_concepts)) THEN
 
-    CALL sp_get_movementstat_data_frommovement(new_account_id,old_category_id,old_custom_date,old_type,v_daysinit,
-                                                   v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
+    CALL sp_add_substract(new_id, old_category_id, old_amount,old_type, old_custom_date, old_duplicated,'MINUS',v_daysinit,v_daysfinal);
 
-    UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat-old_amount) WHERE ms.id=v_id_movement_stat;
+    CALL sp_add_substract(new_id, new_category_id, new_amount,new_type,new_custom_date,new_duplicated,'PLUS',v_daysinit,v_daysfinal);
 
-    CALL sp_get_movementstat_data_frommovement(new_account_id,new_category_id,new_custom_date,new_type,v_daysinit,
-                                                   v_daysfinal,v_userid,v_new_id_movement_stat,v_new_amount_movement_stat);
-
-    IF v_new_amount_movement_stat IS NULL THEN
-
-      CALL sp_get_initandfinaldate ( new_custom_date ,v_daysinit,v_daysfinal, v_initdate,v_finaldate);
-      INSERT INTO movement_stat(user_id,category_id,type,amount,init_date,final_date) 
-      values(v_userid,new_category_id,new_type,new_amount,v_initdate,v_finaldate);
-
-    ELSE
-
-      UPDATE movement_stat ms SET ms.amount=(v_new_amount_movement_stat+new_amount) WHERE ms.id=v_new_id_movement_stat;
-
-    END IF;
-  # IF user change date or type while there are concepts
-   ELSEIF ((v_ch_date OR v_ch_type ) AND (NOT v_ch_has_concepts AND new_has_concepts)) THEN
+  # IF user change date, type, or duplicated  while there are concepts
+   ELSEIF ((v_ch_date OR v_ch_type OR v_ch_duplicated ) AND (NOT v_ch_has_concepts AND new_has_concepts)) THEN
   
     OPEN concept_cursor;
      
@@ -98,21 +61,9 @@ CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_co
       LEAVE iterate_concept;
     END IF;
 
-    CALL sp_get_movementstat_data_frommovement (new_account_id,v_category_id,old_custom_date,old_type,v_daysinit,
-                                                v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
-
-    UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat-v_amount_concept) WHERE ms.id=v_id_movement_stat;
+    CALL sp_add_substract(new_id, v_category_id, v_amount_concept,old_type,old_custom_date,old_duplicated,'MINUS',v_daysinit,v_daysfinal);
             
-    CALL sp_get_movementstat_data_frommovement (new_account_id,v_category_id,new_custom_date,new_type,v_daysinit,
-                                                v_daysfinal,v_userid,vn_id_movement_stat,vn_amount_movement_stat);
-    IF vn_amount_movement_stat IS NULL THEN 
-  
-     CALL sp_get_initandfinaldate ( new_custom_date, v_daysinit, v_daysfinal, v_initdate,v_finaldate);
-        INSERT INTO movement_stat(user_id,category_id,type,amount,init_date,final_date) 
-        values(v_userid,v_category_id,new_type,v_amount_concept,v_initdate,v_finaldate);
-    ELSE 
-      UPDATE  movement_stat ms SET ms.amount=(vn_amount_movement_stat+v_amount_concept) WHERE ms.id=vn_id_movement_stat; 
-     END IF;
+    CALL sp_add_substract(new_id, v_category_id, v_amount_concept,new_type,new_custom_date,new_duplicated,'PLUS',v_daysinit,v_daysfinal);
 
     END LOOP;
     CLOSE concept_cursor;
@@ -122,10 +73,8 @@ CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_co
    #If User delete a movement and does not have concepts.
    IF new_date_deleted IS NOT NULL AND NOT new_has_concepts THEN
 
-    CALL sp_get_movementstat_data_frommovement (new_account_id,new_category_id,new_custom_date,new_type,v_daysinit,
-                                                v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
+   CALL sp_add_substract(new_id, old_category_id, old_amount,old_type,old_custom_date,old_duplicated,'MINUS',v_daysinit,v_daysfinal);
 
-    UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat-new_amount) WHERE ms.id=v_id_movement_stat;
    # If User delete a movement and have concepts
    ELSEIF new_date_deleted IS NOT NULL AND new_has_concepts THEN
 
@@ -141,10 +90,8 @@ CREATE PROCEDURE sp_change_on_movement (IN new_has_concepts bit(1),IN old_has_co
       LEAVE iterate_concept;
     END IF;
 
-    CALL sp_get_movementstat_data_frommovement (new_account_id,v_category_id,old_custom_date,old_type,v_daysinit,
-                                                v_daysfinal,v_userid,v_id_movement_stat,v_amount_movement_stat);
+    CALL sp_add_substract(new_id, v_category_id, v_amount_concept,old_type,old_custom_date,old_duplicated,'MINUS',v_daysinit,v_daysfinal);
 
-    UPDATE movement_stat ms SET ms.amount=(v_amount_movement_stat-v_amount_concept) WHERE ms.id=v_id_movement_stat;
     END LOOP;
     CLOSE concept_cursor;
     
@@ -157,21 +104,16 @@ DROP PROCEDURE IF EXISTS sp_change_on_concept;
 CREATE PROCEDURE sp_change_on_concept(IN new_amount decimal(19,2), IN old_amount decimal(19,2), IN new_category_id varchar(255),
         IN new_movement_id varchar(255),IN new_concept_type varchar(255), IN v_daysinit  int(11), IN v_daysfinal int(11))
   BEGIN
-   DECLARE v_ch_date INT(11);
-   DECLARE v_date datetime;
-   DECLARE v_userid VARCHAR (255);
-   DECLARE v_id VARCHAR (255);
-   DECLARE v_type VARCHAR (255);
-   DECLARE v_amount decimal(19,2);
+
   DECLARE v_ch_amount INT(11);
 
   SET v_ch_amount=new_amount != old_amount;
-
-  CALL sp_get_movement_data(new_movement_id,new_category_id,v_daysinit,v_daysfinal,v_date,v_type,v_userid,v_id,v_amount);
     
   IF (v_ch_amount AND new_concept_type='DEFAULT') THEN 
     
-    UPDATE movement_stat ms SET ms.amount=(v_amount-old_amount+new_amount) WHERE ms.id=v_id;
+    CALL sp_add_substract( new_movement_id, new_category_id, old_amount,null,null,null,'MINUS',v_daysinit,v_daysfinal);
+    CALL sp_add_substract( new_movement_id, new_category_id, new_amount,null,null,null,'PLUS',v_daysinit,v_daysfinal); 
+
   END IF;
 
   END;
--- sql/triggers_movement_stat.sql
+++ sql/triggers_movement_stat.sql
@@ -3,23 +3,22 @@ DROP TRIGGER IF EXISTS t_insert_amount;
 CREATE TRIGGER t_insert_amount AFTER INSERT ON movement
   FOR EACH ROW
   BEGIN
-  CALL sp_insert_amount(NEW.id, NEW.category_id, NEW.amount, 1, 1);#This call is for only one day
-
-
+  CALL sp_add_substract(NEW.id, NEW.category_id, NEW.amount,NEW.type,NEW.custom_date,NEW.duplicated,'PLUS',1,1);
  END;
 
 DROP TRIGGER IF EXISTS t_insert_amount_from_concept;
 CREATE TRIGGER t_insert_amount_from_concept AFTER INSERT ON concept
   FOR EACH ROW
   BEGIN
-  CALL sp_insert_amount (NEW.movement_id, NEW.category_id, NEW.amount, 1, 1);#This call is for only one day
+  CALL sp_add_substract(NEW.movement_id, NEW.category_id, NEW.amount,null,null,null,'PLUS',1,1);
   END;
 DROP TRIGGER IF EXISTS t_update_amount_from_movement;
 CREATE TRIGGER t_update_amount_from_movement AFTER UPDATE ON movement
   FOR EACH ROW
   BEGIN
   CALL sp_change_on_movement ( NEW.has_concepts,OLD.has_concepts,NEW.amount,OLD.amount,NEW.category_id,
-                    OLD.category_id,NEW.type,OLD.type,NEW.custom_date,OLD.custom_date,NEW.account_id,NEW.id,NEW.date_deleted,1,1);
+         OLD.category_id,NEW.type,OLD.type,NEW.custom_date,OLD.custom_date,NEW.account_id,NEW.id, 
+         NEW.date_deleted,NEW.duplicated, OLD.duplicated,1,1);
 
   END;
 
@@ -27,7 +26,7 @@ CREATE TRIGGER t_update_amount_from_movement AFTER UPDATE ON movement
 CREATE TRIGGER t_update_amount_from_concept AFTER UPDATE ON concept
   FOR EACH ROW
   BEGIN
-  CALL sp_change_on_concept (NEW.amount,OLD.amount,NEW.category_id, NEW.movement_id,NEW.type, 1, 1);#This call is for only one day
+  CALL sp_change_on_concept (NEW.amount,OLD.amount,NEW.category_id, NEW.movement_id,NEW.type, 1, 1);
 
   END;
 
@@ -35,8 +34,7 @@ DROP TRIGGER IF EXISTS t_delete_amount;
 CREATE TRIGGER t_delete_amount AFTER DELETE ON concept
   FOR EACH ROW
   BEGIN
-    CALL sp_delete_amount_from_concept( OLD.movement_id,OLD.category_id, OLD.amount,1,1 );
-   
+    CALL sp_add_substract(OLD.movement_id, OLD.category_id, OLD.amount,null,null,null,'MINUS',1,1); 
     END;
 |
 delimiter ;
