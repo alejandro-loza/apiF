50ff379bfca298eaa919fd69d56b4cc960710835
Erick Roberto Mercado Arroyo <erick.mercado@dinerio.mx>
2018-12-04 13:49:13 -0600

    remove find account by id on hsbc

--- src/main/groovy/mx/finerio/api/services/AccountService.groovy
+++ src/main/groovy/mx/finerio/api/services/AccountService.groovy
@@ -199,7 +199,7 @@ class AccountService {
     def institution = credential.institution
     def user = credential.user
     def instance 
-    if ( id ) {
+    if ( id && institution.code != "HSBC" ) {
       instance = accountRepository.findFirstByInstitutionAndUserAndIdBankOrderByDateCreatedDesc(
         institution, user, id )
     }
@@ -222,7 +222,8 @@ class AccountService {
   private Account validateFinder( Credential credential, String id, Account instance){
 
     if ( instance && id ) {
-      if( instance.idBank && instance.idBank != id ){ instance = null }
+      if( credential.institution.code != "HSBC" &&
+          instance.idBank && instance.idBank != id ){ instance = null }
     }
 
     if ( instance?.deleted && instance?.dateDeleted && 
