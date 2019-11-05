3daa858bb2ac2582a6a8c03bc61f3ddecb643977
Jose Antonio <jose.vargas@finerio.mx>
2019-11-05 12:25:53 -0600

    changing the authentication to scrapper for logging

--- src/main/groovy/mx/finerio/api/services/DevScraperService.groovy
+++ src/main/groovy/mx/finerio/api/services/DevScraperService.groovy
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import groovy.json.*
 
 @Service
 class DevScraperService {
@@ -20,6 +27,9 @@ class DevScraperService {
   @Value( '${scraper.credentials.path}' )
   String credentialsPath
 
+  @Value( '${scraper.credentials.encodedCredentials}' )
+  String encodedCredentials
+
   @Autowired
   RestTemplateService restTemplateService
 
@@ -27,19 +37,30 @@ class DevScraperService {
   Map requestData( Map data ) throws Exception {
 
     def finalUrl = "${url}/${credentialsPath}"
-    def headers = [ 'Authorization': "Bearer ${login().authorizationToken}" ]
+    def headers = [ 'Authorization': "Bearer ${login().access_token}" ]
     def body = [ data: [ data ] ]
     restTemplateService.post( finalUrl, headers, body )
 
   }
 
-  private Map login() throws Exception {
+ private Map login() throws Exception{
+    
+    OkHttpClient client = new OkHttpClient()
+    MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded")
+    RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&scope=all")
+    Request request = new Request.Builder()
+      .url("${url}/${credentialsPath}")
+      .post(body)
+      .addHeader("Content-Type", "application/x-www-form-urlencoded")
+      .addHeader("Authorization", "Basic ${loginCredentials}")
+      .build()
 
-    def finalUrl = "${url}/${loginPath}"
-    def headers = [ 'Authorization': "Basic ${loginCredentials}" ]
-    def params = [:]
-    restTemplateService.get( finalUrl, headers, params )
-
+    Response response = client.newCall(request).execute()
+    def responseString = response.body().string()
+    def statusCode = response.code()
+    if( statusCode != 200 ){ throw new Exception( responseString ) }   
+    def res = new JsonSlurper().parseText( responseString ?: '{}' )  
+    res
   }
 
 }
