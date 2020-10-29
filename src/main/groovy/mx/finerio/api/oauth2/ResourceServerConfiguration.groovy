b12c92da70c9966c20f4f6bde2a07a53e945da58
Alejandro García <alejandro.garcia@finerio.mx>
2020-10-29 12:58:17 -0600

    Added CORS policy

--- src/main/groovy/mx/finerio/api/oauth2/ResourceServerConfiguration.groovy
+++ src/main/groovy/mx/finerio/api/oauth2/ResourceServerConfiguration.groovy
@@ -1,11 +1,16 @@
 package mx.finerio.api.oauth2
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableResourceServer
@@ -15,6 +20,7 @@ class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
   void configure( HttpSecurity http ) throws Exception {
 
     http
+      .cors().and()
       .authorizeRequests()
       .antMatchers( '/callbacks/**' ).permitAll()
       .antMatchers( '/password/**' ).permitAll()
@@ -29,4 +35,15 @@ class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
     resources.resourceId( 'resource_id' )
   }
 
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+
+    def source = new UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration( '/**',
+        new CorsConfiguration().applyPermitDefaultValues() )
+    return source
+
+  }
+
 }
+
