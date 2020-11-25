a089ba8dbb8924bd799e5a4c3fa7388abed72020
Jose Antonio <jose.vargas@finerio.mx>
2020-11-25 09:11:36 -0600

    Minor fix

--- src/main/groovy/mx/finerio/api/services/MyUserDetailsService.groovy
+++ src/main/groovy/mx/finerio/api/services/MyUserDetailsService.groovy
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import mx.finerio.api.exceptions.UserBlockedException
 
 @Service
 class MyUserDetailsService implements UserDetailsService {
