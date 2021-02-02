a079dc067212c2eb04eb4da04041edab44827959
Jose Antonio <jose.vargas@finerio.mx>
2021-02-02 11:28:36 -0600

    Adding functionality of ScraperV2 service

---
+++ src/main/groovy/mx/finerio/api/aop/ScraperV2ServiceCreateCredential.groovy
@@ -0,0 +1,49 @@
+package mx.finerio.api.aop
+
+import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.annotation.AfterThrowing
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import org.springframework.stereotype.Component
+import mx.finerio.api.dtos.CreateCredentialDto
+
+@Component
+@Aspect
+class ScraperV2ServiceCreateCredential{
+
+  final static Logger log = LoggerFactory.getLogger(
+      'mx.finerio.api.aop.ScraperV2ServiceCreateCredential' )
+
+  @Pointcut(
+    value='execution(void mx.finerio.api.services.ScraperV2Service.createCredential(..)) && bean(scraperV2Service) && args(createCredentialDto)',
+    argNames='createCredentialDto'
+  )
+  public void createCredential( CreateCredentialDto createCredentialDto ) {}
+
+  @Before('createCredential(createCredentialDto)')
+  void before(CreateCredentialDto createCredentialDto) {
+    log.info( "<< createCredentialDto: {}", createCredentialDto )
+  }
+
+  @AfterReturning(
+    pointcut='createCredential(mx.finerio.api.dtos.CreateCredentialDto)'
+    
+  )
+  void afterReturning() {
+    log.info( '>> ok')
+  }
+
+  @AfterThrowing(
+    pointcut='createCredential(mx.finerio.api.dtos.CreateCredentialDto)',
+    throwing='e'
+  )
+  void afterThrowing( Exception e ) {
+    log.info( "XX ${e.class.simpleName} - ${e.message}" )
+  }
+
+}
---
+++ src/main/groovy/mx/finerio/api/dtos/CreateCredentialDto.groovy
@@ -0,0 +1,13 @@
+package mx.finerio.api.dtos
+
+import groovy.transform.ToString
+
+@ToString(includeNames = true, includePackage = false)
+class CreateCredentialDto {
+
+  String bankCode  
+  String username
+  String password
+  String credentialId
+  
+}
