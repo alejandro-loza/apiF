a7c68036f84b4ee77a68afd0173e7c0b11a08700
Jose Antonio <jose.vargas@finerio.mx>
2021-04-21 18:57:00 -0500

    Unit testting an file fix relocation

---
+++ src/test/groovy/mx/finerio/api/services/FinancialInstitutionServiceFindOneByCodeSpec.groovy
@@ -0,0 +1,60 @@
+package mx.finerio.api.services
+
+import mx.finerio.api.domain.Client
+import mx.finerio.api.domain.FinancialInstitution
+import mx.finerio.api.domain.repository.FinancialInstitutionRepository
+import mx.finerio.api.dtos.FinancialInstitutionDto
+import mx.finerio.api.exceptions.BadImplementationException
+import mx.finerio.api.exceptions.InstanceNotFoundException
+
+import spock.lang.Specification
+
+class FinancialInstitutionServiceFindOneByCodeSpec extends Specification {
+
+  def service = new FinancialInstitutionService()
+
+  def financialInstitutionRepository = Mock( FinancialInstitutionRepository )
+
+  def setup() {
+    service.financialInstitutionRepository = financialInstitutionRepository
+  }
+
+  def "invoking method successfully"() {
+
+    when:
+      def result = service.findOneByCode( code )
+    then:
+      1 * financialInstitutionRepository.findOneByCode( _ as String ) >>
+          new FinancialInstitution()
+      result instanceof FinancialInstitution
+    where:
+      code = 'bnx'
+
+  }
+
+  def "parameter 'code' is null"() {
+
+    when:
+      service.findOneByCode( code )
+    then:
+      BadImplementationException e = thrown()
+      e.message == 'financialInstitutionService.findOneByCode.code.null'
+    where:
+      code = null
+
+  }
+
+  def "instance not found"() {
+
+    when:
+      service.findOneByCode( code )
+    then:
+         1 * financialInstitutionRepository.findOneByCode( _ as String ) >> null
+      InstanceNotFoundException e = thrown()
+      e.message == 'financialInstitution.not.found'
+    where:
+       code = 'bnx'
+
+  }
+
+}
