8752f87af7ecea10a5dff1ced9bc9b958f9af395
Pinky <alejandro.hernandez@finerio.mx>
2021-04-30 14:12:08 -0500

    added aop diagnosis service

---
+++ src/main/groovy/mx/finerio/api/aop/DiagnosisServiceGetDiagnosisByCustomer.groovy
@@ -0,0 +1,43 @@
+package mx.finerio.api.aop
+
+import mx.finerio.api.dtos.SummaryDto
+import org.aspectj.lang.annotation.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+@Aspect
+class DiagnosisServiceGetDiagnosisByCustomer {
+
+  final static Logger log = LoggerFactory.getLogger(
+      'mx.finerio.api.aop.DiagnosisServiceGetDiagnosisByCustomer' )
+
+  @Pointcut(
+    value='execution(mx.finerio.api.dtos.DiagnosisDto mx.finerio.api.services.DiagnosisService.getDiagnosisByCustomer(..)) && bean(diagnosisService) && args(customerId, averageManualIncome)',
+    argNames='customerId, averageManualIncome'
+  )
+  void getDiagnosisByCustomer( Long customerId , Optional averageManualIncome) {}
+
+  @Before('getDiagnosisByCustomer(customerId, averageManualIncome)')
+  void before( Long customerId, Optional averageManualIncome ) {
+    log.info( "<< customerId: {}, averageManualIncome: {}", customerId, averageManualIncome.get() )
+  }
+
+  @AfterReturning(
+    pointcut='getDiagnosisByCustomer(java.lang.Long, java.util.Optional)',
+    returning='response'
+  )
+  void afterReturning( SummaryDto response ) {
+    log.info( '>> response: {}', response )
+  }
+
+  @AfterThrowing(
+    pointcut='getDiagnosisByCustomer(java.lang.Long, java.util.Optional)',
+    throwing='e'
+  )
+  void afterThrowing( Exception e ) {
+    log.info( "XX ${e.class.simpleName} - ${e.message}" )
+  }
+
+}
